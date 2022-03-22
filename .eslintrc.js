53d15c2165745bb5f58ff63423accb799fe83dc5
Saiki K <pacexy@gmail.com>
2022-03-22 20:45:48 +0800

    chore: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ['next', 'prettier'],
+  plugins: ['@typescript-eslint'],
+  settings: {
+    next: {
+      rootDir: ['apps/*/', 'packages/*/'],
+    },
+  },
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'import/order': [
+      'error',
+      {
+        alphabetize: { order: 'asc' },
+        'newlines-between': 'always',
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+      },
+    ],
+  },
+}
