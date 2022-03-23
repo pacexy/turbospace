1eac60f9aff2b491b4701a41b14347e75d1346c3
Saiki K <pacexy@gmail.com>
2022-03-23 11:41:05 +0800

    feat: empty layout

--- apps/web/pages/_app.page.tsx
+++ apps/web/pages/_app.page.tsx
@@ -2,6 +2,12 @@ import './styles.css'
 
 import { AppProps } from 'next/app'
 
+import Layout from './Layout'
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
