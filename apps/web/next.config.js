b72f15e41cd0892f1abdf99c0c2587e4533d105e
Saiki K <pacexy@gmail.com>
2022-04-03 15:05:34 +0800

    feat: add `ts-library`

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,7 @@ const withPWA = require('next-pwa')
 const withTM = require('next-transpile-modules')([
   '@turbospace/internal',
   '../packages/react-library',
+  '../packages/ts-library',
 ])
 
 /**
