8f13dc851a768a3e1ce6c66957ef755b27a0fef1
Saiki K <pacexy@gmail.com>
2022-04-13 17:47:31 +0800

    refactor(next): use `AppShell`

--- apps/next/src/components/Layout.tsx
+++ apps/next/src/components/Layout.tsx
@@ -5,7 +5,9 @@ import {
   NavDrawer,
   NavBar,
   Button,
+  AppShell,
 } from '@literal-ui/core'
+import { Link } from '@literal-ui/next'
 import { MdCheck } from 'react-icons/md'
 import { RiGithubFill } from 'react-icons/ri'
 
@@ -29,52 +31,58 @@ const items = [
 
 export const Layout: React.FC = ({ children }) => {
   return (
-    <>
-      <TopAppBar
-        leading={
-          <TopAppBar.Leading>
-            <NavDrawer.Toggler />
-          </TopAppBar.Leading>
-        }
-        headline={<TopAppBar.Title>Turbospace</TopAppBar.Title>}
-        trailing={
-          <TopAppBar.Trailing>
-            <Button variant="text">Log in</Button>
-            <Button variant="tonal">Sign up</Button>
-            <IconButton
-              renderAs="a"
-              href="https://github.com/pacexy/turbospace"
-              Icon={RiGithubFill}
-            />
-            <ColorScheme />
-          </TopAppBar.Trailing>
-        }
-      />
-
-      <div className="flex">
+    <AppShell
+      className="mx-auto max-w-3xl p-4"
+      header={
+        <TopAppBar
+          leading={
+            <TopAppBar.Leading>
+              <NavDrawer.Toggler />
+            </TopAppBar.Leading>
+          }
+          headline={<TopAppBar.Title>Turbospace</TopAppBar.Title>}
+          trailing={
+            <TopAppBar.Trailing>
+              <Button className="hidden sm:block" variant="text">
+                Log in
+              </Button>
+              <Button className="hidden sm:block" variant="tonal">
+                Sign up
+              </Button>
+              <IconButton
+                as={Link}
+                href="https://github.com/pacexy/turbospace"
+                Icon={RiGithubFill}
+              />
+              <ColorScheme />
+            </TopAppBar.Trailing>
+          }
+        />
+      }
+      sidebar={
         <NavDrawer>
-          {sections.map(({ name, items }) => (
-            <NavDrawer.Block key={name} headline={name}>
-              {items.map((item) => (
-                <NavDrawer.Item key={item}>
-                  <a>{item}</a>
+          {sections.map(({ name, items }, i) => (
+            <NavDrawer.Section key={i} headline={name}>
+              {items.map((item, j) => (
+                <NavDrawer.Item key={j}>
+                  <Link href={`/`}>{item}</Link>
                 </NavDrawer.Item>
               ))}
-            </NavDrawer.Block>
+            </NavDrawer.Section>
           ))}
         </NavDrawer>
-        <main className="mx-auto w-screen max-w-3xl flex-1 p-4">
-          {children}
-        </main>
-      </div>
-
-      <NavBar>
-        {items.map(({ Icon, name }) => (
-          <NavBar.Item key={name} Icon={Icon}>
-            <a>{name}</a>
-          </NavBar.Item>
-        ))}
-      </NavBar>
-    </>
+      }
+      navbar={
+        <NavBar className="sm:hidden">
+          {items.map(({ Icon, name }, i) => (
+            <NavBar.Item key={i} Icon={Icon}>
+              <a>{name}</a>
+            </NavBar.Item>
+          ))}
+        </NavBar>
+      }
+    >
+      {children}
+    </AppShell>
   )
 }
